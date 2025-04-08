6be374f964a0e6b385ab57327174289e55d0a925
Kitan <kitan@goodthingco.xyz>
2025-04-08 21:09:02 +0100

    fix error

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserTable from './user/UserTable'
+
+const page = () => {
+  return (
+      <div>
+          <UserTable/>
+    </div>
+  )
+}
+
+export default page

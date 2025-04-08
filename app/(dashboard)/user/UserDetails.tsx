ed3b32cadebb4838f7f1c9e7a60dc60ac924e7b3
Kitan <kitan@goodthingco.xyz>
2025-04-08 21:22:07 +0100

    fix the client side

--- app/(dashboard)/user/UserDetails.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserDetails = () => {
-  return (
-    <div className='text-red-400'>UserDetails</div>
-  )
-}
-
-export default UserDetails
--- app/(dashboard)/user/page.tsx
+++ app/(dashboard)/user/page.tsx
@@ -1,41 +1,9 @@
-"use client";
+import React from 'react'
 
-import { useEffect, useState } from "react";
-import Card from "@/components/Cards";
-import React from "react";
-import UserTable from "./UserTable";
-
-interface CardProps {
-  title: string;
-  icon: string;
-  count: number;
+const UserDetails = () => {
+  return (
+    <div className='text-red-400'>UserDetails</div>
+  )
 }
 
-const UserList = () => {
-  const [cards, setCards] = useState<CardProps[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/cards")
-      .then((response) => response.json())
-      .then((data) => setCards(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  return (
-    <div className=" p-6 pt-4">
-      <h2 className="text-[var(--royalblue)] text-2xl font-semibold mb-6">Users</h2>
-      <div className="grid grid-cols-4 gap-6 space-y-3">
-        {cards.map((card, index) => (
-          <div key={index} className="">
-            <Card title={card.title} icon={card.icon} count={card.count} />
-          </div>
-        ))}
-      </div>
-      <div>
-        <UserTable/>
-      </div>
-    </div>
-  );
-};
-
-export default UserList;
+export default UserDetails
